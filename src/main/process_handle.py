52f1ba14dd15abdf54306d64622580a97a7a8b4e
Diogo <diogopelinsonduartemoraes@gmail.com>
2026-01-26 01:27:37 -0300

    Inital commit: Inicinado a arquitetura mvc com a criacao do constructor e a main

---
+++ src/main/process_handle.py
@@ -0,0 +1,12 @@
+#Apontar para os construtores que forem sendo feitos
+
+from .constructor.introduction_process import introduction_process
+
+def start() -> None:
+    while True:
+        command = introduction_process()
+
+        if command == '1': print('Comando 1 foi acionado')
+        elif command == '2': print('Comando 2 foi acionado')
+        elif command == '5': exit()
+        else: print('\n Comando nao encontrado!! \n\n')
