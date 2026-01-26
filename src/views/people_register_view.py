9145d223572cc57ff3501d314e755745c954e971
Diogo <diogopelinsonduartemoraes@gmail.com>
2026-01-26 02:36:19 -0300

    feat: Adicionando o controller do registro de pessoa

--- src/views/people_register_view.py
+++ src/views/people_register_view.py
@@ -3,6 +3,7 @@ from typing import Dict
 
 class PeopleRegisterView:
     #Metodo
+    #Pagina inicial da feature
     def registry_person_view(self) -> Dict:
         os.system('cls||clear')
 
@@ -19,3 +20,29 @@ class PeopleRegisterView:
         }
 
         return new_person_informations
+    
+    #Pagina de sucesso
+    def registry_person_sucess(self, message: Dict) -> None:
+        os.system('cls||clear')
+
+        sucess_message = f'''
+            Usuario Cadastrado com sucesso!
+
+            Tipo: { message["type"] }
+            Registros: { message["count"] }
+            Infos: 
+                Nome: { message["attributes"]["name"] }
+                Idade: { message["attributes"]["age"] }
+        '''
+        print(sucess_message)
+
+    #Pagina da falha
+    def registry_person_fail(self, error: str) -> None:
+        os.system('cls||clear')
+
+        fail_message = f'''
+            Falha ao Cadastrar usuario!
+
+            Erro: { error }
+        '''
+        print(fail_message)
