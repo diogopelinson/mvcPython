05c5acb11d7bec670514fd6a96f0525f90f0a901
Diogo <diogopelinsonduartemoraes@gmail.com>
2026-02-03 19:20:32 -0300

    feat: criando a controller do finder

--- src/main/constructor/people_finder_constructor.py
+++ src/main/constructor/people_finder_constructor.py
@@ -1,11 +1,14 @@
 from src.views.people_finder_view import PeopleFinderView
-
+from src.controllers.people_finder_controller import PeopleFinderController
 
 def people_finder_constructor():
     people_finder_view = PeopleFinderView()
-    # Controller a baixo
+    people_finder_controller = PeopleFinderController()
 
     person_finder_informations = people_finder_view.find_person_view()
-    # Enviar para o Controller
+    response = people_finder_controller.find_by_name(person_finder_informations)
 
-
+    if response["success"]:
+        people_finder_view.find_person_success(response["message"])
+    else:
+        people_finder_view.find_person_fail(response["erorr"])
